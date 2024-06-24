3fb6451b748c0ce20cd85e3d90a15cb3d3a1d35d
Bastian Gabrielli <gabriellidoren@gmail.com>
2024-06-23 20:48:50 -0400

    mejora en la estructura coodigo 4colorScript

--- assets/js/4colorScript.js
+++ assets/js/4colorScript.js
@@ -3,7 +3,13 @@ function cambiarBgNegro(event) {
   event.target.style.backgroundColor = "black";
 }
 
-document.getElementById('blue').addEventListener('click', cambiarBgNegro);
-document.getElementById('red').addEventListener('click', cambiarBgNegro);
-document.getElementById('green').addEventListener('click', cambiarBgNegro);
-document.getElementById('yellow').addEventListener('click', cambiarBgNegro);
+
+const div1 = document.getElementById("blue");
+const div2 = document.getElementById("red");
+const div3 = document.getElementById("green");
+const div4 = document.getElementById("yellow");
+
+div1.addEventListener('click', cambiarBgNegro);
+div2.addEventListener('click', cambiarBgNegro);
+div3.addEventListener('click', cambiarBgNegro);
+div4.addEventListener('click', cambiarBgNegro);
