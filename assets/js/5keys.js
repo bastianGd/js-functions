33608096187642ca451c92fbe0cacf3ce922b8dc
Bastian Gabrielli <gabriellidoren@gmail.com>
2024-06-23 22:50:03 -0400

    mejora en el código

--- assets/js/5keys.js
+++ assets/js/5keys.js
@@ -8,33 +8,26 @@ function changeDivBgColors(color) {
 //Función para crear el nuevo elemento
 function newDivElement(color) {
   const newDiv = document.createElement("div");
-  newDiv.style.border = "solid 1px black";
-  newDiv.style.width = "200px";
-  newDiv.style.height = "200px";
   newDiv.style.backgroundColor = color;
+  newDiv.setAttribute("class", "dynamic-div")
   document.body.appendChild(newDiv);
 }
 //cambio color para key
-document.addEventListener("keydown", function (event) {
-  if (document.getElementById("key"))
-    switch (event.key) {
-      case "a":
-        changeDivBgColors("pink");
-        break;
-      case "s":
-        changeDivBgColors("orange");
-        break;
-      case "d":
-        changeDivBgColors("lightblue");
-        break;
+document.addEventListener("keydown", function(event) {
+  if (document.getElementById("key")) {
+    if (event.key === "a") {
+      changeDivBgColors("pink");
+    } else if (event.key === "s") {
+      changeDivBgColors("orange");
+    } else if (event.key === "d") {
+      changeDivBgColors("lightblue");
+    } else if (event.key === "q") {
+      newDivElement("purple");
+    } else if (event.key === "w") {
+      newDivElement("grey");
+    } else if (event.key === "e") {
+      newDivElement("brown");
     }
-});
-document.addEventListener("keydown", function (event) {
-  if (event.key === "q") {
-    newDivElement("purple");
-  } else if (event.key == "w") {
-    newDivElement("grey");
-  } else if (event.key == "e") {
-    newDivElement("brown");
   }
 });
+
